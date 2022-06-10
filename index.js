4486b55bf26d3e7c720bbb649316f3d5e2264dfa
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-06-10 16:12:43 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+// Write your code here!main.remove(main);
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+victory.innerHTML = "<h1>Ian is the champion</h1>";
